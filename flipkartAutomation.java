488ac19f6a85b390752621dea4117116ea94b779
unknown <sdetmindc126@iiht.tech>
2021-12-16 09:36:10 +0530

    changes in Second

--- flipkartAutomation.java
+++ flipkartAutomation.java
@@ -17,7 +17,7 @@ public class flipkartAutomation{
 public static void main(String[] args) throws InterruptedException {
 	System.setProperty("webdriver.chrome.driver","E:\\chromedriver.exe");
 	WebDriver driver=new ChromeDriver();
-	  //Navigate to flipkart Website
+	  //Navigate to flipkart WEBSITE
 
   driver.get("http://www.flipkart.com/");
   driver.manage().window().maximize();
